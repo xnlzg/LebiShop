d9d66750d246478ae7ce12026cc14cc1502a5b6c
luozeguang <78059047@qq.com>
2020-02-26 16:28:01 +0800

    '添加单点登录'

--- Shop.Bussiness/EX_Admin.cs
+++ Shop.Bussiness/EX_Admin.cs
@@ -5,6 +5,7 @@ using System.Text;
 using Shop.Model;using DB.LebiShop;
 using LB.Tools;
 using System.Collections.Specialized;
+using System.IO;
 using System.Web;
 using System.Web.SessionState;
 using System.Security.Cryptography;
@@ -204,7 +205,59 @@ namespace Shop.Bussiness
             strin = BitConverter.ToString(hashmd5.ComputeHash(Encoding.Default.GetBytes(strin))).Replace("-", "").ToLower();
             return strin;
         }
+        /// <summary>
+        /// DES加密
+        /// </summary>
+        /// <param name="data">加密数据</param>
+        /// <param name="key">8位字符的密钥字符串</param>
+        /// <param name="iv">8位字符的初始化向量字符串</param>
+        /// <returns></returns>
+        public static string DesEncrypt(string data, string key= "lzg@1112", string iv="lzg!1234")
+        {
+            byte[] byKey = Encoding.ASCII.GetBytes(key);
+            byte[] byIv = Encoding.ASCII.GetBytes(iv);
 
+            DESCryptoServiceProvider cryptoProvider = new DESCryptoServiceProvider();
+            int i = cryptoProvider.KeySize;
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cst = new CryptoStream(ms, cryptoProvider.CreateEncryptor(byKey, byIv), CryptoStreamMode.Write);
+
+            StreamWriter sw = new StreamWriter(cst);
+            sw.Write(data);
+            sw.Flush();
+            cst.FlushFinalBlock();
+            sw.Flush();
+            return Convert.ToBase64String(ms.GetBuffer(), 0, (int)ms.Length);
+        }
+
+        /// <summary>
+        /// DES解密
+        /// </summary>
+        /// <param name="data">解密数据</param>
+        /// <param name="key">8位字符的密钥字符串(需要和加密时相同)</param>
+        /// <param name="iv">8位字符的初始化向量字符串(需要和加密时相同)</param>
+        /// <returns></returns>
+        public static string DesDecrypt(string data, string key = "lzg@1112", string iv = "lzg!1234")
+        {
+            byte[] byKey = Encoding.ASCII.GetBytes(key);
+            byte[] byIv = Encoding.ASCII.GetBytes(iv);
+
+            byte[] byEnc;
+            try
+            {
+                byEnc = Convert.FromBase64String(data);
+            }
+            catch
+            {
+                return null;
+            }
+
+            DESCryptoServiceProvider cryptoProvider = new DESCryptoServiceProvider();
+            MemoryStream ms = new MemoryStream(byEnc);
+            CryptoStream cst = new CryptoStream(ms, cryptoProvider.CreateDecryptor(byKey, byIv), CryptoStreamMode.Read);
+            StreamReader sr = new StreamReader(cst);
+            return sr.ReadToEnd();
+        }
         #endregion
 
         #region 管理员权限
--- Shop/pagebase/page/P_Index.cs
+++ Shop/pagebase/page/P_Index.cs
@@ -15,7 +15,70 @@ namespace Shop
     {
         protected override void LoadPage(string themecode, int siteid, string languagecode, string pcode)
         {
+
+            #region 单点登录 xnlzg 测试用户kingdge登录 (http://localhost:23712/?sign=tNneZFTdxyHM7BHwHKENhNWEerWb1Jhr)
+
+            //var signTemp = EX_Admin.DesEncrypt("kingdge≌123456");
+            var sign = Rstring("sign");
+            if (!string.IsNullOrWhiteSpace(sign))
+            {
+                //解密
+                var nameAndPass = EX_Admin.DesDecrypt(sign).Split('≌');
+                var userName = nameAndPass[0];
+                var password = nameAndPass[1];
+                //判断用户是否存在,不存在创建新帐号
+                int count = B_Lebi_User.Counts("UserName=lbsql{'" + userName + "'}");
+                if (count == 0)
+                {
+                    Lebi_UserLevel defaultlevel = B_Lebi_UserLevel.GetModel("Grade>0 order by Grade asc");
+                    Lebi_User model = new Lebi_User();
+                    B_Lebi_User.SafeBindForm(model);
+                    model.NickName = model.UserName;
+                    model.Password = EX_User.MD5(password);
+                    model.UserLevel_id = defaultlevel.id;
+                    model.Time_Reg = DateTime.Now;
+                    model.Time_This = DateTime.Now;
+                    model.Time_Last = DateTime.Now;
+                    model.Count_Login = 1;
+                    model.IP_This = RequestTool.GetClientIP();
+                    model.IP_Last = RequestTool.GetClientIP(); 
+                    model.User_id_parent = 0;
+                    if (CurrentSite != null)
+                        model.Site_id = CurrentSite.id;
+                    if (CurrentLanguage != null)
+                        model.Language = CurrentLanguage.Code;
+                    model.Area_id = RequestTool.RequestInt("Area_id", 0);
+                    int DefaultUserEndDays = 0;
+                    int.TryParse(SYS.DefaultUserEndDays, out DefaultUserEndDays);
+                    model.Time_End = System.DateTime.Now.AddDays(DefaultUserEndDays);
+                    Lebi_User user = EX_User.UserReg(model, password);
+                }
+                //登录
+                if (EX_User.UserLogin(userName, password))
+                {
+                    CurrentUser = EX_User.CurrentUser();
+                    if (ShopCache.GetBaseConfig().IsOpenUserEnd == "1")
+                    {
+                        if (CurrentUser.Time_End < System.DateTime.Now)
+                        {
+                            Response.Write("{\"msg\":\"" + Tag("账号已过期") + "\"}");
+                            CookieTool.DeleteCookie("User");
+                            return;
+                        }
+                    } 
+                    if (SYS.Verifycode_UserLogin == "1")
+                    {
+                        HttpContext.Current.Session["loginerror"] = "false";
+                    } 
+                } 
+
+            }
+
+
+            #endregion
+
             LoadTheme(themecode, siteid, languagecode, pcode);
+
         }
         public override string ThemePageMeta(string code, string tag)
         {
